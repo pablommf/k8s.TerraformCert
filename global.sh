954f7c06d9c7453c410952d4b91d127f792dea84
Pablo Maximiliano Minervino Foltyn <pablo.minervino@globant.com>
2021-02-24 09:15:00 -0300

    ideas to automate

---
+++ global.sh
@@ -0,0 +1,11 @@
+#the intention of this script is call in proper sequence each separated step of the creation  k8s cluster
+
+#TBD: languaje: Python, Ansible or Bash
+
+
+
+
+
+#4 Deploy Terraform
+
+#5 install kubeconfile files
